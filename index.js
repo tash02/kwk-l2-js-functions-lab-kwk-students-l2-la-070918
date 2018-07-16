191f53c1a5d4889227f06ddb4127de109a1ba6a9
tash02 <tash.brinkley@gmail.com>
2018-07-16 17:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(somevalue){
+  if (somevalue>= 42) {
+  result1 = (somevalue - 42)
+  return result1
+  }
+  else if (somevalue< 42) {
+    result1 = (42 - somevalue)
+    return result1
+  }
+}
+function distanceFromHqInFeet(somevalue) {
+  return distanceFromHqInBlocks(somevalue) * 264
+}
